1d46180d1e874b1ce465fa42f4ef2412d9497537
Yoan Laurain <yoan.laurain0@gmail.com>
2023-09-21 22:02:11 +0200

    Project base

---
+++ MauiTuto/MauiTuto/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTuto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiTuto/MauiTuto/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTuto
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
